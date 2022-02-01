7a0c601d27305efc05534944f0feaf9a63856b0a
Victor Manuel Lajara Andreu <victormlajara03@gmail.com>
2022-02-01 21:22:50 +0100

    migration 50/50

--- database/migrations/2022_02_01_185142_create_article_table.php
+++ database/migrations/2022_02_01_185142_create_article_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateArticleTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,15 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('article', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('propriety_id')->references('id')->on('users');
+            $table->foreignId('buyer_id')->nullable()->references('id')->on('users');
+            $table->foreignId('category_id')->references('id')->on('category');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('description');
+            $table->double('price');
             $table->string('location');
-            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -32,6 +33,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('article');
     }
 }
--- database/migrations/2022_02_01_192010_create_report-a_table.php
+++ database/migrations/2022_02_01_192010_create_report-a_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateReportATable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,10 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('report_article', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('location');
-            $table->rememberToken();
+            $table->foreignId('article_id')->references('id')->on('article');
+            $table->string('description')->nullable();
             $table->timestamps();
         });
     }
@@ -32,6 +28,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('report_article');
     }
 }
